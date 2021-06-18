e16f05f165eec1a8bb79c3430b20993ae3adec32
ksillah <kutah.sillah@gmail.com>
2021-06-17 22:10:36 -0500

    working on rows

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -8,6 +8,9 @@ import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
 import { useEffect } from 'react';
+import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
+import { useDemoData } from '@material-ui/x-grid-data-generator';
+
 //const cors=require('cors');
 
 
@@ -26,6 +29,7 @@ const countries = [
   },
 ];
 
+
 const useStyles = makeStyles(theme =>({
   main: {
     width: '100%',
@@ -35,7 +39,7 @@ const useStyles = makeStyles(theme =>({
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     color: '#002699',
-    position: 'fixed',
+    //position: 'fixed',
     textAlign: 'center',
     paddingTop: '250px',
 },
@@ -50,7 +54,31 @@ const spotifyApi= new SpotifyWebApi({
 })
 
 export default function Dashboard({code} ) {
+  const rows: GridRowsProp = [
+    { id: 1, col1: '' , col2: '', col3: '' },
+    { id: 2, col1: '', col2: '', col3: '' },
+    { id: 3, col1: '', col2: '', col3: '' },
+    { id: 4, col1: '', col2: '', col3: '' },
+    { id: 5, col1: '', col2: '', col3: '' },
+    { id: 6, col1: '', col2: '', col3: '' },
+    { id: 7, col1: '', col2: '', col3: '' },
+    { id: 8, col1: '', col2: '', col3: '' },
+    { id: 9, col1: '', col2: '', col3: '' },
+    { id: 10, col1: '', col2: '', col3: '' },
+    { id: 11, col1: '', col2: '', col3: '' },
+    { id: 12, col1: '', col2: '', col3: '' },
+    { id: 13, col1: '', col2: '', col3: '' },
+    { id: 14, col1: '', col2: '', col3: '' },
+    { id: 15, col1: '', col2: '', col3: '' },
+
+  ];
+  const columns: GridColDef[] = [
+{ field: 'col1', headerName: 'Artist', width: 150 },
+{ field: 'col2', headerName: 'Song', width: 150 },
+{field: 'col3', headerName: 'Preview', width: 150 },
+];
     var [playlist_id, setPlaylist_id] = useState('');
+    var result: string[]=[];
     const accessToken=useAuth(code);
     console.log(accessToken)
     const classes= useStyles();
@@ -87,28 +115,36 @@ export default function Dashboard({code} ) {
           function(data) {
             console.log('made it')
           console.log('The playlist contains these tracks', data.body);
+          for (var i = 0; i < 15; i++) {
+            var artist=data.body.items[i].track.artists[0].name
+          console.log(artist)
+          var track=data.body.items[i].track.name
+          console.log(track)
+          var preview=data.body.items[i].track.preview_url
+          console.log(preview)
+          var val=[artist,track,preview]
+          result.push(val)
+          //rows[i]={id:i, col1: artist , col2: track, col3: preview}
+          console.log(rows[i])
+          }
+          console.log(result)
           })
+          
     }, [accessToken, value,playlist_id])
 
+    var { data } = useDemoData({
+      dataSet: 'Commodity',
+      rowLength: 10,
+      maxColumns: 6,
+    });
     
-
-    // useEffect(()=>{
-    //   if (!value) return
-    //   spotifyApi.setAccessToken(accessToken)
-    //   if (!accessToken) return
-    //   console.log('HERE:',playlist_id)
-    //   console.log(accessToken)
-    //   spotifyApi.getPlaylistTracks({playlist_id},{
-    //     fields: 'items'
-    //   }).then(
-    //   function(data) {
-    //   console.log(playlist_id)
-    //   console.log('The playlist contains these tracks', data.body);
-    //   })
-  
-    // }, [accessToken, value, playlist_id])
+          // var newtag =document.createElement('h4')
+          // newtag.append(val)
+          // var element= document.getElementById('new');
+          // element.appendChild(newtag);
 
     return (
+      
       <div>
       <AppBar  position="fixed" style= {{paddingLeft: '15px' }}>
                 <h1>Sounds of the World</h1>
@@ -127,7 +163,11 @@ export default function Dashboard({code} ) {
           
           
           <h1>you chose {value}</h1>
-          
+            
+          <div style={{ height: 300, width: '100%' }}>
+            <DataGrid checkboxSelection rows={rows} columns={columns} />
+          </div>
+
         </div>
       </div>
 
