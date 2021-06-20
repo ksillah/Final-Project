94007fa618f5b46a1ad5395f0f28593a34c84085
ksillah <kutah.sillah@gmail.com>
2021-06-19 21:34:50 -0500

    added uri column

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -59,6 +59,7 @@ export default function Dashboard({code} ) {
     var [artists,setArtist]= useState([]);
     var [tracks,setTrack]= useState([]);
     var [previews,setPreview]= useState([]);
+    var[uris,setUris]=useState([]);
 
     //Gets the user selected country
     const accessToken=useAuth(code);
@@ -71,18 +72,12 @@ export default function Dashboard({code} ) {
     useEffect(() =>{
       if (!accessToken) return
       spotifyApi.setAccessToken(accessToken)
-      setArtist([]);
-      setTrack([]);
-      setPreview([]);
     }, [accessToken])
 
     useEffect(()=>{
       if (!value) return
       if (!accessToken) return
       spotifyApi.setAccessToken(accessToken)
-      setArtist([]);
-      setTrack([]);
-      setPreview([]);
       console.log(value)
       spotifyApi.searchPlaylists("Top 50 - "+ value).then(function(data) {//finds the top 50 playlist for the selected country
         setPlaylist_id( data.body.playlists.items[0].id)
@@ -102,20 +97,26 @@ export default function Dashboard({code} ) {
           artists=[]
           tracks=[]
           previews=[]
+          uris=[]
           console.log('made it')
           console.log('The playlist contains these tracks', data.body);
           for (var i = 0; i < 25; i++) {
             var artist=data.body.items[i].track.artists[0].name
             var track=data.body.items[i].track.name
             var preview= data.body.items[i].track.preview_url
-            var val=[[artist],[track],[preview]]
+            if(preview==null){
+              preview=data.body.items[i].track.uri
+              }
+            var uri= data.body.items[i].track.uri
             previews.push(preview)
             artists.push(artist)
             tracks.push(track)
+            uris.push(uri)
         }
           setArtist(artists);
           setTrack(tracks);
           setPreview(previews)
+          setUris(uris)
           })
           
     }, [accessToken, value,playlist_id])
@@ -123,12 +124,15 @@ export default function Dashboard({code} ) {
     // let ready=false
     // console.log('ready',ready)
     let output=''
+    let my_id=''
     function createPlaylist() {
       //if (output=='') return
       spotifyApi.setAccessToken(accessToken)
       spotifyApi.createPlaylist( `My ${value} playlist`, { 'description': `My favorite songs from the top 25 songs in ${value} right now`, 'public': true })
       .then(function(data) {
-      console.log('Created playlist!');
+      console.log('Created playlist!', data);
+      my_id=data.body.id
+      console.log('playlist_id', my_id)
       }, function(err) {
         console.log('Something went wrong!', err);
         return;
@@ -139,39 +143,38 @@ export default function Dashboard({code} ) {
     
     //structure of table
     const rows: GridRowsProp = [
-      { id: 1, col1: artists[0] , col2: tracks[0], col3: previews[0]},
-      { id: 2, col1: artists[1] , col2: tracks[1], col3: previews[1] },
-      { id: 3, col1: artists[2] , col2: tracks[2], col3: previews[2] },
-      { id: 4, col1: artists[3] , col2: tracks[3], col3: previews[3] },
-      { id: 5, col1: artists[4] , col2: tracks[4], col3: previews[4] },
-      { id: 6, col1: artists[5] , col2: tracks[5], col3: previews[5] },
-      { id: 7, col1: artists[6] , col2: tracks[6], col3: previews[6] },
-      { id: 8, col1: artists[7] , col2: tracks[7], col3: previews[7] },
-      { id: 9, col1: artists[8] , col2: tracks[8], col3: previews[8] },
-      { id: 10, col1: artists[9] , col2: tracks[9], col3: previews[9] },
-      { id: 11, col1: artists[10] , col2: tracks[10], col3: previews[10] },
-      { id: 12, col1: artists[11] , col2: tracks[11], col3: previews[11] },
-      { id: 13, col1: artists[12] , col2: tracks[12], col3: previews[12] },
-      { id: 14, col1: artists[13] , col2: tracks[13], col3: previews[13] },
-      { id: 15, col1: artists[14] , col2: tracks[14], col3: previews[14] },
-      { id: 16, col1: artists[15] , col2: tracks[15], col3: previews[15] },
-      { id: 17, col1: artists[16] , col2: tracks[16], col3: previews[16] },
-      { id: 18, col1: artists[17] , col2: tracks[17], col3: previews[17] },
-      { id: 19, col1: artists[18] , col2: tracks[18], col3: previews[18] },
-      { id: 20, col1: artists[19] , col2: tracks[19], col3: previews[19] },
-      { id: 21, col1: artists[20] , col2: tracks[20], col3: previews[20] },
-      { id: 22, col1: artists[21] , col2: tracks[21], col3: previews[21] },
-      { id: 23, col1: artists[22] , col2: tracks[22], col3: previews[22] },
-      { id: 24, col1: artists[23] , col2: tracks[23], col3: previews[23] },
-      { id: 25, col1: artists[24] , col2: tracks[24], col3: previews[24] },
-
-  
+      { id: 1, col1: artists[0] , col2: tracks[0], col3: previews[0], col4: uris[0]},
+      { id: 2, col1: artists[1] , col2: tracks[1], col3: previews[1], col4: uris[1] },
+      { id: 3, col1: artists[2] , col2: tracks[2], col3: previews[2], col4: uris[2] },
+      { id: 4, col1: artists[3] , col2: tracks[3], col3: previews[3], col4: uris[3] },
+      { id: 5, col1: artists[4] , col2: tracks[4], col3: previews[4], col4: uris[4] },
+      { id: 6, col1: artists[5] , col2: tracks[5], col3: previews[5], col4: uris[5] },
+      { id: 7, col1: artists[6] , col2: tracks[6], col3: previews[6], col4: uris[6] },
+      { id: 8, col1: artists[7] , col2: tracks[7], col3: previews[7], col4: uris[7] },
+      { id: 9, col1: artists[8] , col2: tracks[8], col3: previews[8], col4: uris[8] },
+      { id: 10, col1: artists[9] , col2: tracks[9], col3: previews[9], col4: uris[9] },
+      { id: 11, col1: artists[10] , col2: tracks[10], col3: previews[10], col4: uris[10] },
+      { id: 12, col1: artists[11] , col2: tracks[11], col3: previews[11], col4: uris[11] },
+      { id: 13, col1: artists[12] , col2: tracks[12], col3: previews[12], col4: uris[12] },
+      { id: 14, col1: artists[13] , col2: tracks[13], col3: previews[13], col4: uris[13] },
+      { id: 15, col1: artists[14] , col2: tracks[14], col3: previews[14], col4: uris[14] },
+      { id: 16, col1: artists[15] , col2: tracks[15], col3: previews[15], col4: uris[15] },
+      { id: 17, col1: artists[16] , col2: tracks[16], col3: previews[16], col4: uris[16] },
+      { id: 18, col1: artists[17] , col2: tracks[17], col3: previews[17], col4: uris[17] },
+      { id: 19, col1: artists[18] , col2: tracks[18], col3: previews[18], col4: uris[18] },
+      { id: 20, col1: artists[19] , col2: tracks[19], col3: previews[19], col4: uris[19] },
+      { id: 21, col1: artists[20] , col2: tracks[20], col3: previews[20], col4: uris[20] },
+      { id: 22, col1: artists[21] , col2: tracks[21], col3: previews[21], col4: uris[21] },
+      { id: 23, col1: artists[22] , col2: tracks[22], col3: previews[22], col4: uris[22] },
+      { id: 24, col1: artists[23] , col2: tracks[23], col3: previews[23], col4: uris[23] },
+      { id: 25, col1: artists[24] , col2: tracks[24], col3: previews[24], col4: uris[24] },
     ];
     const columns: GridColDef[] = [
   { field: 'col1', headerName: 'Artist', width: 200 },
   { field: 'col2', headerName: 'Song', width: 300 },
   {field: 'col3', headerName: 'Preview', width: 200, hide:true},
-  {field: 'col4', headerName: 'Button', width:200,
+  {field:'col4', headerName:'Uri', width:200},
+  {field: 'col5', headerName: 'Button', width:200,
   renderCell: (params: GridCellParams) => (//creates preview link
     <strong>
       <a style={{ color:'blue'}}  href={params.getValue(params.id,'col3')} target="_blank">
@@ -184,7 +187,7 @@ export default function Dashboard({code} ) {
 //determines if the data table should be shown
   
   //console.log('tracks3', tracks)
-  if (tracks.length==25) {
+  if (tracks.length>0) {
     output = <DataGrid checkboxSelection rows={rows} columns={columns} />;
     // ready=true
     // console.log('ready',ready)
